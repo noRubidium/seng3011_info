9e75a621e16a1b2d81e31e830f22f71c87edec0d
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-30 14:57:56 +1100

    much better support

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@
 // and new IE Edge outputs to true now for window.chrome
 // and if not iOS Chrome check
 // so use the below updated condition
-var isChromium = window.chrome,
+const isChromium = window.chrome,
     winNav = window.navigator,
     vendorName = winNav.vendor,
     isOpera = winNav.userAgent.indexOf("OPR") > -1,
@@ -16,12 +16,13 @@ if (isIOSChrome) {
 } else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
    // is Google Chrome
 } else { 
-   $("body").html("<h1>PLEASE USE CHROME</h1>");
+   $(".container").html("<h1>PLEASE USE CHROME</h1>");
 }
 
 if (window.location.protocol === 'https:') {
-    $("body").html("<h1>PLEASE GO TO OUR NEW RELEASE PAGE: <p></p> <a href='http://release.kaiworship.xyz'>Eleven 51 API release</a></h1>");
+    $(".container").html("<h1>PLEASE GO TO OUR NEW RELEASE PAGE: <p></p> <a href='http://release.kaiworship.xyz'>Eleven 51 API release</a></h1>");
 }
+
 const commodities = ['Total',
     'Food',
     'HouseholdGood',
