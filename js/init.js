35c21def06af809dc59609c04d8a14bbd0da3bb1
Minjie Shen <mshen@cse.unsw.edu.au>
2017-03-30 11:31:20 +1100

    Please use chrome

--- js/init.js
+++ js/init.js
@@ -1,3 +1,24 @@
+// please note, 
+// that IE11 now returns undefined again for window.chrome
+// and new Opera 30 outputs true for window.chrome
+// and new IE Edge outputs to true now for window.chrome
+// and if not iOS Chrome check
+// so use the below updated condition
+var isChromium = window.chrome,
+    winNav = window.navigator,
+    vendorName = winNav.vendor,
+    isOpera = winNav.userAgent.indexOf("OPR") > -1,
+    isIEedge = winNav.userAgent.indexOf("Edge") > -1,
+    isIOSChrome = winNav.userAgent.match("CriOS");
+
+if(isIOSChrome){
+   // is Google Chrome on IOS
+} else if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+   // is Google Chrome
+} else { 
+   $(".section").html("<h1>PLEASE USE CHROME</h1>");
+}
+
 const commodities = ['Total',
     'Food',
     'HouseholdGood',
